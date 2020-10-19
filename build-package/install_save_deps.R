2b10fc8eacdc7a5d27480c058496c717d071b508
Jan Caha <jan.caha@outlook.com>
2020-10-19 22:33:09 +0200

    try to build pkgdown site

--- build-package/install_save_deps.R
+++ build-package/install_save_deps.R
@@ -1,5 +1,5 @@
 # install packages necessary to build the package code
-install.packages(c("remotes", "desc", "glue", "readr", "purrr", "stringr", "here", "knitr", "rmarkdown", "devtools"))
+install.packages(c("remotes", "desc", "glue", "readr", "purrr", "stringr", "here", "knitr", "rmarkdown", "devtools", "pkgdown"))
 remotes::install_github("paleolimbot/qgisprocess")
 
 # save files withe dependencies
