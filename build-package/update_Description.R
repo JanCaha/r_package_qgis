d611eb478fac0d0f16996bc70b7074141a321b36
Jan Caha <jan.caha@outlook.com>
2020-10-20 22:25:14 +0200

    update version only if code actually changed

--- build-package/update_Description.R
+++ build-package/update_Description.R
@@ -3,10 +3,16 @@ library(glue)
 library(desc)
 library(qgisprocess)
 
+files_changed_from_last_run <- readr::read_rds(here::here("data-raw", "changed_files.rds"))
+
 qgis_version <- qgisprocess::qgis_version()
 
 description <- glue::glue("An extension of package `qgisprocess` providing direct R functions for QGIS functions. Compiled against QGIS version: {qgis_version}.")
 desc_file <- desc::desc()
 desc_file$set("Description", description)
-desc_file$bump_version("minor")
+
+if (!length(files_changed_from_last_run) == 0){
+  desc_file$bump_version("minor")
+}
+
 desc_file$write()
