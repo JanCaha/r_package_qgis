8491c564ad801ef0813ad2bc39b6ff1300de8908
Jan Caha <jan.caha@outlook.com>
2021-05-02 23:05:25 +0200

    add roxygen tags

--- build-package/build_utils.R
+++ build-package/build_utils.R
@@ -2,10 +2,14 @@ function_check_QGIS_versions <- function(){
 
   qgis_build_version <- stringr::str_match(qgisprocess::qgis_version(), "[0-9.]+[[:cntrl:][:alnum:]]")[,1]
 
-  fn_code <- glue::glue("check_QGIS_versions <- function(){{\n",
+  fn_code <- glue::glue("#' @importFrom glue glue\n",
+                        "#' @importFrom stringr str_match\n",
+                        "#' @importFrom utils compareVersion \n",
+                        "\n",
+                        "check_QGIS_versions <- function(){{\n",
                         "  qgis_build <- \"{qgis_build_version}\"\n",
                         "  qgis_local <- stringr::str_match(qgisprocess::qgis_version(), \"[0-9.]+[[:cntrl:][:alnum:]]\")[,1]\n",
-                        "  if (compareVersion(qgis_local, qgis_build) < 0) {{\n",
+                        "  if (utils::compareVersion(qgis_local, qgis_build) < 0) {{\n",
                         "    message(glue::glue(\"The QGIS version running localy ({{qgis_local}}) is older then the QGIS version against which the package was build ({{qgis_build}}).\"))\n",
                         "  }}\n",
                         "}}")
