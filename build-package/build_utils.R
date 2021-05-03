f50a2e110f0117652dfe4f19982a66a3865a1489
Jan Caha <jan.caha@outlook.com>
2021-05-03 17:03:07 +0200

    process for building utils.R for package

--- build-package/build_utils.R
+++ build-package/build_utils.R
@@ -1,20 +1,13 @@
-function_check_QGIS_versions <- function(){
+# variable for template
+qgis_build_version <- stringr::str_match(qgisprocess::qgis_version(), "[0-9.]+[[:cntrl:][:alnum:]]")[,1]
 
-  qgis_build_version <- stringr::str_match(qgisprocess::qgis_version(), "[0-9.]+[[:cntrl:][:alnum:]]")[,1]
+# load function template
+fun_tmpl <- readr::read_file("build-package/templates/fun_onLoad.R")
 
-  fn_code <- glue::glue("#' @importFrom glue glue\n",
-                        "#' @importFrom stringr str_match\n",
-                        "#' @importFrom utils compareVersion \n",
-                        "\n",
-                        "check_QGIS_versions <- function(){{\n",
-                        "  qgis_build <- \"{qgis_build_version}\"\n",
-                        "  qgis_local <- stringr::str_match(qgisprocess::qgis_version(), \"[0-9.]+[[:cntrl:][:alnum:]]\")[,1]\n",
-                        "  if (utils::compareVersion(qgis_local, qgis_build) < 0) {{\n",
-                        "    message(glue::glue(\"The QGIS version running localy ({{qgis_local}}) is older then the QGIS version against which the package was build ({{qgis_build}}).\"))\n",
-                        "  }}\n",
-                        "}}")
-}
+# evaluate function template against existing variables
+fun_code <- glue::glue(fun_tmpl)
 
-readr::write_file(glue::glue_collapse(list(function_check_QGIS_versions()),
+readr::write_file(glue::glue_collapse(list(fun_code,
+                                           readr::read_file("build-package/functions/check_algorithm_necessities.R")),
                                       sep = "\n\n"),
                   glue::glue("./R/utils.R"))
