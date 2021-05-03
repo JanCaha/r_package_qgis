c75086758383c88a3ae936c3945757fef6eef241
Jan Caha <jan.caha@outlook.com>
2021-05-03 22:11:11 +0200

    verify also alg provider

--- build-package/functions/check_algorithm_necessities.R
+++ build-package/functions/check_algorithm_necessities.R
@@ -2,5 +2,12 @@ check_algorithm_necessities <- function(alg_name){
 
   qgisprocess::assert_qgis()
 
+  provider <- stringr::str_split(alg_name, ":", simplify = TRUE)[1]
+
+  if (!qgisprocess::qgis_has_provider(provider)){
+    stop(glue::glue("Error: Can not find specified provider `{provider}` in local QGIS installation."))
+  }
+
   qgisprocess::assert_qgis_algorithm(alg_name)
+
 }
