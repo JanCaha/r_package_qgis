f5ecc6900171c1764190963c815cc72f2e050b0e
Jan Caha <jan.caha@outlook.com>
2021-05-03 22:12:05 +0200

    on load fails if QGIS is not acessible

--- build-package/templates/fun_onLoad.R
+++ build-package/templates/fun_onLoad.R
@@ -4,6 +4,10 @@
 
 .onLoad <- function(libname, pkgname){{
 
+  if (!qgisprocess::assert_qgis()){
+    stop("Cannot load package `qgis` because working QGIS installation cannot be found. See help for `qgisprocess::qgis_configure()`!")
+  }
+
   qgis_build <- "{qgis_build_version}"
 
   qgis_local <- stringr::str_match(qgisprocess::qgis_version(), "[0-9.]+[[:cntrl:][:alnum:]]")[,1]
