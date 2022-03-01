e472a366e7a1a32eb1133e69ad68d046eb405ed1
Jan Caha <jan.caha@outlook.com>
2022-03-01 16:58:22 +0100

    add parameters to functions

--- build-package/templates/qgis_function.R
+++ build-package/templates/qgis_function.R
@@ -1,8 +1,14 @@
-{fn_name} <- function({fn_arguments},..., .complete_output = TRUE) {{
+{fn_name} <- function({fn_arguments},..., .complete_output = .complete_output_option(), .quiet = .quiet_option(), .messages = .message_option()) {{
 
   check_algorithm_necessities("{alg$algorithm}")
 
-  output <- qgisprocess::qgis_run_algorithm("{alg$algorithm}", {fn_arguments_qgis_run},...)
+  if (messages){{
+    output <- qgisprocess::qgis_run_algorithm("{alg$algorithm}", {fn_arguments_qgis_run},..., .quiet = .quiet)
+  }} else {{
+    suppressMessages(
+      output <- qgisprocess::qgis_run_algorithm("{alg$algorithm}", {fn_arguments_qgis_run},..., .quiet = .quiet)
+      )
+  }}
 
   if (.complete_output) {{
     return(output)
