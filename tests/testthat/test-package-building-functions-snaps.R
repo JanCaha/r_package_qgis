c3ae01bf7632aa2a7929e63a5724b16eb4e0581a
Jan Caha <jan.caha@outlook.com>
2022-03-01 11:01:18 +0100

    add tests for bulding files

---
+++ tests/testthat/test-package-building-functions-snaps.R
@@ -0,0 +1,42 @@
+# Tests for code and help generation for specific selected QGIS functions
+
+test_that("Functions build code and doc tessellate", {
+
+  alg_help <- get_alg_help("tessellate")
+
+  expect_snapshot_file(save_txt(alg_help$doc), name = "tesselate_help.txt")
+  expect_snapshot_file(save_txt(alg_help$code), name = "tesselate_code.txt")
+})
+
+test_that("Functions build code and doc retile", {
+
+  alg_help <- get_alg_help("retile")
+
+  expect_snapshot_file(save_txt(alg_help$doc), name = "retile_help.txt")
+  expect_snapshot_file(save_txt(alg_help$code), name = "retile_code.txt")
+})
+
+test_that("Functions build code and doc i.cluster", {
+
+  alg_help <- get_alg_help("i.cluster")
+
+  expect_snapshot_file(save_txt(alg_help$doc), name = "i_cluster_help.txt")
+  expect_snapshot_file(save_txt(alg_help$code), name = "i_cluster_code.txt")
+})
+
+test_that("Functions build code and doc r.sim.water", {
+
+  alg_help <- get_alg_help("r.sim.water")
+
+  expect_snapshot_file(save_txt(alg_help$doc), name = "r_sim_water_help.txt")
+  expect_snapshot_file(save_txt(alg_help$code), name = "r_sim_water_code.txt")
+})
+
+test_that("Functions build code and doc analyticalhierarchyprocess", {
+
+  alg_help <- get_alg_help("analyticalhierarchyprocess")
+
+  expect_snapshot_file(save_txt(alg_help$doc), name = "analyticalhierarchyprocess_help.txt")
+  expect_snapshot_file(save_txt(alg_help$code), name = "analyticalhierarchyprocess_code.txt")
+
+})
---
+++ tests/testthat/test-package-building-functions.R
@@ -0,0 +1,76 @@
+# Test internal package building functions
+
+test_that("Functions fix parameter names", {
+
+  source(here::here("build-package", "functions_fix_parameter_names.R"))
+
+  argument <- replace_starting_minus("-m")
+
+  expect_equal(argument, ".m")
+
+  argument <- remove_asterisk("arg*")
+
+  expect_equal(argument, "arg")
+
+  argument <- replace_spaces("arg name")
+
+  expect_equal(argument, "arg_name")
+
+  argument <- add_arg_to_numeric_arguments("-1")
+
+  expect_equal(argument, "-arg_1")
+
+  argument <- add_arg_to_numeric_arguments("1")
+
+  expect_equal(argument, "arg_1")
+
+  argument <- add_arg_to_reserverd_keywords("function")
+
+  expect_equal(argument, "arg_function")
+
+})
+
+test_that("Functions fix_description", {
+
+  source(here::here("build-package", "functions_fix_parameter_names.R"))
+
+  argument <- remove_brackets("text [a]")
+
+  expect_equal(argument, "text (a)")
+
+  argument <- remove_brackets("text <a>")
+
+  expect_equal(argument, "text a")
+
+})
+
+test_that("Functions fix_description", {
+
+  source(here::here("build-package", "functions_fix_parameter_names.R"))
+
+  argument <- convert_to_R_arg_names("-m*")
+
+  expect_equal(argument, ".m")
+
+  argument <- convert_to_R_arg_names("1")
+
+  expect_equal(argument, "arg_1")
+
+  argument <- convert_to_R_arg_names("for a")
+
+  expect_equal(argument, "for_a")
+
+  argument <- convert_to_R_arg_names("for")
+
+  expect_equal(argument, "arg_for")
+})
+
+test_that("Functions fix_description", {
+
+  source(here::here("build-package", "functions_fix_algorithm_id.R"))
+
+  alg_id <- fix_algorithm_id("grass7.r.do_something")
+
+  expect_equal(alg_id, "grass7_r_do_something")
+
+})
