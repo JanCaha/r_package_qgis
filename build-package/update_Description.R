482075a926ba64726ad1ec8f116e9d393924b810
Jan Caha <jan.caha@outlook.com>
2022-03-02 09:29:02 +0100

    change version style

--- build-package/update_Description.R
+++ build-package/update_Description.R
@@ -6,12 +6,22 @@ files_changed_from_last_run <- readr::read_rds(here::here("data-raw", "changed_f
 
 qgis_version <- qgisprocess::qgis_version()
 
+qgis_version_numeric <- stringr::str_extract(qgis_version, "[0-9]+\\.[0-9]+\\.[0-9]+")
+
 description <- glue::glue("An extension of package `qgisprocess` providing direct R functions for QGIS functions. Compiled against QGIS version: {qgis_version}.")
 desc_file <- desc::desc()
 desc_file$set("Description", description)
 
-if (!length(files_changed_from_last_run) == 0){
-  desc_file$bump_version("minor")
+current_version <- desc_file$get("Version")
+
+dev_version <- stringr::str_extract(current_version, "[0-9]+$")
+
+if (stringr::str_detect(current_version, qgis_version_numeric)){
+  desc_file$bump_version("dev")
+} else {
+  version_number <- glue::glue("{qgis_version_numeric}.{dev_version}")
+  desc_file$set("Version", qgis_version_numeric)
+  desc_file$bump_version("dev")
 }
 
 desc_file$write()
