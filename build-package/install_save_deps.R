0b9830f701ff037ae702de957f90173435c03a4f
Jan Caha <jan.caha@outlook.com>
2023-06-28 21:41:20 +0200

    move install to pak

--- build-package/install_save_deps.R
+++ build-package/install_save_deps.R
@@ -1,8 +1,8 @@
 # install packages necessary to build the package code
-install.packages(c("remotes", "desc", "glue", "readr", "purrr", "stringr", "here",
-                   "fs", "knitr", "rmarkdown", "devtools", "pkgdown", "progress", "dplyr"))
+# install.packages(c("remotes", "desc", "glue", "readr", "purrr", "stringr", "here",
+#                    "fs", "knitr", "rmarkdown", "devtools", "pkgdown", "progress", "dplyr"))
 
-remotes::install_github("r-spatial/qgisprocess", upgrade="ask")
+# remotes::install_github("r-spatial/qgisprocess", upgrade="ask")
 
 # save files withe dependencies
 saveRDS(remotes::dev_package_deps(dependencies = TRUE), ".github/depends.Rds", version = 2)
